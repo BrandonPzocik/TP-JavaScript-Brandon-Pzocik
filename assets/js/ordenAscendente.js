ac0f73a56cdc29ec3a3dc64b659678ab8721710f
Brandon <pzocikbrandon@gmail.com>
2024-05-03 11:47:02 -0700

    se termino el ejercicio de parImpar

---
+++ assets/js/ordenAscendente.js
@@ -0,0 +1,15 @@
+//Escribe un programa que pida al usuario tres números y luego los muestre
+//en orden ascendente.
+
+let num1 = parseInt(prompt("ingrese el primer número:"))
+let num2 = parseInt(prompt("ingrese el segundo número:"))
+let num3 = parseInt(prompt("ingrese el tercer número:"))
+if (num1 < num2 && num1 < num3 && num2 < num3) {
+    console.log(num1, num2, num3)
+} else if (num2 < num1 && num2 < num3 && num1 < num3 ) {
+    console.log(num2, num1, num3)
+}else  if (num3 < num1 && num3 < num2 && num2 < num1) {
+    console.log(num3, num2, num1)
+} else {
+    console.log(num1, num3, num3)
+}
---
+++ assets/js/parImpar.js
@@ -0,0 +1,10 @@
+//Escriba un programa que pida al usuario un número y luego muestre si es
+//par o impar.
+
+let numero = prompt("ingrese un número")
+
+if (numero % 2 === 0){
+    console.log("El número es PAR")
+} else{
+    console.log("El número es IMPAR")
+}
