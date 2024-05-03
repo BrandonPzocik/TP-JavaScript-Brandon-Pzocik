e489cddcf50ab1badcd7738a2443a2796fd3d308
Brandon <pzocikbrandon@gmail.com>
2024-05-03 10:23:42 -0700

    se termino el ejercicio de rango de edad

--- assets/js/edadRango.js
+++ assets/js/edadRango.js
@@ -2,13 +2,17 @@
 //rango de edad se encuentra: niño (0-12), adolescente (13-19), adulto
 //(20-59), o adulto mayor (60 o más).
 
-let edad = prompt(pasrsefloat("ingrese su edad"))
+let edad = parseInt(prompt("ingrese su edad"));
 
-if (edad >= 0 || edad<=12) {
+//if (edad < 0){
+//    console.log("ingrese una edad valida")
+//}
+
+if (edad >= 0 && edad<=12) {
     console.log("Usted es un NIÑO")
-} else if (edad >= 13 || edad <=19) { 
+} else if (edad >= 13 && edad <=19) { 
     console.log("Usted es un ADOLESCENTE")
-} else if (edad >= 20 || edad <=59) {
+} else if (edad >= 20 && edad <=59) {
     console.log("Ustede es un ADULTO")
 } else if ( edad >= 60){
     console.log("Usted es un ADULTO MAYOR ")
