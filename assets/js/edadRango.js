4253be9ece51439b38c6b48ea75ece239d7375f2
Brandon <pzocikbrandon@gmail.com>
2024-05-03 09:47:29 -0700

    Se creo el index.html

---
+++ assets/js/edadRango.js
@@ -0,0 +1,17 @@
+//Escribe un programa que pida al usuario su edad y luego le diga en qué
+//rango de edad se encuentra: niño (0-12), adolescente (13-19), adulto
+//(20-59), o adulto mayor (60 o más).
+
+let edad = prompt(pasrsefloat("ingrese su edad"))
+
+if (edad >= 0 || edad<=12) {
+    console.log("Usted es un NIÑO")
+} else if (edad >= 13 || edad <=19) { 
+    console.log("Usted es un ADOLESCENTE")
+} else if (edad >= 20 || edad <=59) {
+    console.log("Ustede es un ADULTO")
+} else if ( edad >= 60){
+    console.log("Usted es un ADULTO MAYOR ")
+} else {
+    console.log("Ingrese un numero valido")
+}
